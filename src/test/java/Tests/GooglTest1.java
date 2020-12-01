432f8eda629f5a72d9f490f1e49c6cf16239eec8
MIKHAIL_KATYSHEV <katyshevmikhail@yandex.ru>
2020-12-01 22:41:09 +0400

    Добавлен обход капчи Google

--- src/test/java/Tests/GooglTest1.java
+++ src/test/java/Tests/GooglTest1.java
@@ -1,16 +1,34 @@
 package Tests;
 
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 
 public class GooglTest1 {
+
+    @BeforeAll
+    public static void init() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--user-data-dir=C:\\Users\\Bjoerndalen\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1");
+        options.addArguments("--profile-directory=Profile 1");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://google.com");
+    }
+
 
     @Test
     public void test1() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://google.com");
+        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("selenium", Keys.ENTER);
         driver.quit();
     }
 }
