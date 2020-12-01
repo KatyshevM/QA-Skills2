b776b981cc4399c1285f3051e082c1c0824482d8
MIKHAIL_KATYSHEV <katyshevmikhail@yandex.ru>
2020-12-01 23:24:02 +0400

    Тест2. Нахождение 10 поисков. Создание @BeforeAll

--- src/test/java/Tests/GooglTest1.java
+++ src/test/java/Tests/GooglTest1.java
@@ -9,26 +9,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class GooglTest1 {
+
+    private static WebDriver driver;
 
     @BeforeAll
     public static void init() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximazed");
         options.addArguments("--user-data-dir=C:\\Users\\Bjoerndalen\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1");
         options.addArguments("--profile-directory=Profile 1");
-        WebDriver driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
         driver.get("http://google.com");
     }
 
 
     @Test
     public void test1() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         driver.get("http://google.com");
         driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("selenium", Keys.ENTER);
+        assertEquals(10, driver.findElements(By.cssSelector("#search .g")).size());
         driver.quit();
     }
 }
