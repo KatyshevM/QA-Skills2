b6b2baa56f87e05e2c7dd5e3bdcefcc7cdf30705
MIKHAIL_KATYSHEV <katyshevmikhail@yandex.ru>
2020-12-02 21:58:09 +0400

    Перенос в main

--- src/test/java/Tests/GooglTest1.java
+++ src/test/java/Tests/GooglTest1.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import pages.SearchPage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GooglTest1 {
 
     private static WebDriver driver;
+    private static SearchPage searchPage;
 
     @BeforeAll
     public static void init() {
@@ -26,16 +28,24 @@ public class GooglTest1 {
         options.addArguments("--profile-directory=Profile 1");
         driver = new ChromeDriver(options);
         driver.get("http://google.com");
+        searchPage = new SearchPage(driver);
     }
 
 
     @Test
     public void test1() {
         driver.get("http://google.com");
-        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("selenium", Keys.ENTER);
-        assertEquals(9, driver.findElements(By.cssSelector("#search .g")).size());
+        searchPage.search("selenium");
+        assertEquals(9, searchPage.results.size());
 
     }
+
+    public void test2() {
+
+
+    }
+
+
     @AfterAll
     public static void teardown() {
         driver.quit();
