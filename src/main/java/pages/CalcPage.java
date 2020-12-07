b3c955a09aad70f293661c02dca7af35947d8b88
MIKHAIL_KATYSHEV <katyshevmikhail@yandex.ru>
2020-12-07 22:24:24 +0400

    Выполнено задание 1

---
+++ src/main/java/pages/CalcPage.java
@@ -0,0 +1,78 @@
+package pages;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
+
+public class CalcPage {
+
+    //поиск 1
+    @FindBy(css = "div[jsname='N10B9']")
+    public WebElement oneButton;
+
+    //поиск 2
+    @FindBy(css = "div[jsname='lVjWed']")
+    public WebElement twoButton;
+
+    //поиск 3
+    @FindBy(css = "div[jsname='KN1kY']")
+    public WebElement threeButton;
+
+    //поиск 4
+    @FindBy(css = "div[jsname='xAP7E']")
+    public WebElement fourButton;
+
+    //поиск 5
+    @FindBy(css = "div[jsname='Ax5wH']")
+    public WebElement fiveButton;
+
+    //поиск 0
+    @FindBy(css = "div[jsname='bkEvMb']")
+    public WebElement zeroButton;
+
+    //поиск Открывающаяся скобка
+    @FindBy(css = "div[jsname='j93WEe']")
+    public WebElement openBracket;
+
+    //поиск Закрывающаяся скобка
+    @FindBy(css = "div[jsname='qCp9A']")
+    public WebElement closeBracket;
+
+    //поиск Плюс
+    @FindBy(css = "div[jsname='XSr6wc']")
+    public WebElement plusButton;
+
+    //поиск Минус
+    @FindBy(css = "div[jsname='pPHzQc']")
+    public WebElement minusButton;
+
+    //поиск Умножение
+    @FindBy(css = "div[jsname='YovRWb']")
+    public WebElement multButton;
+
+    //поиск Деление
+    @FindBy(css = "div[jsname='WxTTNd']")
+    public WebElement splitButton;
+
+    //поиск Равно
+    @FindBy(css = "div[jsname='Pt8tGc']")
+    public WebElement equalsButton;
+
+    //поиск Строки памяти
+    @FindBy(css = "span[jsname='ubtiRe']")
+    public WebElement memoryPane;
+
+    //поиск Строки результата
+    @FindBy(css = "span[jsname='VssY5c']")
+    public WebElement resultPane;
+
+
+    public CalcPage(WebDriver driver) {
+        initElements(driver, this);
+
+    }
+}
--- src/test/java/Tests/CalcTests.java
+++ src/test/java/Tests/CalcTests.java
@@ -1,22 +1,23 @@
 package Tests;
 
-
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import pages.DetailsPage;
+import pages.CalcPage;
 import pages.SearchPage;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-public class GooglTest1 {
+public class CalcTests {
 
     private static WebDriver driver;
     private static SearchPage searchPage;
-    private static DetailsPage detailsPage;
+    private static CalcPage calcPage;
+
 
     @BeforeAll
     public static void init() {
@@ -28,35 +29,35 @@ public class GooglTest1 {
         driver = new ChromeDriver(options);
         driver.get("http://google.com");
         searchPage = new SearchPage(driver);
-        detailsPage = new DetailsPage(driver);
+
+
     }
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         driver.get("http://google.com");
     }
 
     @Test
     @DisplayName("Поиск числа резултатов") //Отображение названия теста
     public void test1() {
-        searchPage.search("selenium");
-        assertEquals(9, searchPage.results.size());
-    }
-
-    @Test
-    @DisplayName("Проверка деталей ресурса") //Отображение названия теста
-    public void test2() {
-        searchPage.search("selenium");
-        assertAll(
-                () -> assertEquals("Selenium", detailsPage.getName()),
-                () -> assertEquals("3.141.59 (14 ноября 2018 года)", detailsPage.getLasVersion()),
-                () -> assertEquals("Apache License 2.0", detailsPage.getLicense())
-        );
-    }
-
-    @AfterAll
-    public static void teardown() { driver.quit();
-    }
-
+        searchPage.search("Калькулятор");
+        calcPage = new CalcPage(driver);
+        calcPage.openBracket.click();
+        calcPage.oneButton.click();
+        calcPage.plusButton.click();
+        calcPage.twoButton.click();
+        calcPage.closeBracket.click();
+        calcPage.multButton.click();
+        calcPage.threeButton.click();
+        calcPage.minusButton.click();
+        calcPage.fourButton.click();
+        calcPage.zeroButton.click();
+        calcPage.splitButton.click();
+        calcPage.fiveButton.click();
+        calcPage.equalsButton.click();
+        assertEquals("(1 + 2) × 3 - 40 ÷ 5 =",calcPage.memoryPane.getText());
+        assertEquals("1",calcPage.resultPane.getText());
+        }
 
 }
